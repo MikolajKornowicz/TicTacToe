92932890b3395649f44db491aca75050b7cdf58c
Mikolaj Kornowicz <mikolaj.kornowicz@gmail.com>
2021-10-26 23:16:18 +0200

    back

--- src/main/java/com/kodilla/tictactoe/TicTacToeApplication.java
+++ src/main/java/com/kodilla/tictactoe/TicTacToeApplication.java
@@ -1,13 +1,35 @@
 package com.kodilla.tictactoe;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
 
-@SpringBootApplication
-public class TicTacToeApplication {
+
+public class TicTacToeApplication extends Application {
+    private Image imageback = new Image("file:src/main/resources/background.jpg");
 
     public static void main(String[] args) {
-        SpringApplication.run(TicTacToeApplication.class, args);
+        launch(args);
     }
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        BackgroundSize backgroundSize = new BackgroundSize(100, 100, true, true, true, false);
+        BackgroundImage backgroundImage = new BackgroundImage(imageback, BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize);
+        Background background = new Background(backgroundImage);
+
+        GridPane grid = new GridPane();
+        grid.setBackground(background);
+
+        Scene scene = new Scene(grid, 1600, 900, Color.BLACK);
+
+        primaryStage.setTitle("Tic Tac Toe");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 }
